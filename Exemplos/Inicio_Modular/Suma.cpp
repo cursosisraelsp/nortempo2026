e9d432143505017d5b56f3f739c49e42eaa7a51e
cursosisraelsp <cursos.israelsp@gmail.com>
2026-06-25 16:31:18 +0200

    Exemplos

---
+++ Exemplos/Inicio_Modular/Suma.cpp
@@ -0,0 +1,18 @@
+// Online C++ Compiler - Build, Compile and Run your C++ programs online in your favorite browser
+
+#include<iostream>
+
+using namespace std;
+
+// DECLARACIÓN DE FUNCIONES
+
+int Suma();
+
+int main()
+{
+    cout << "A Suma é: " << Suma();
+}
+
+int Suma(){
+    return 3 + 14;
+}
