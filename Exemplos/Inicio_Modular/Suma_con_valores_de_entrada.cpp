3375577ce78cbddf543ae6afe4308a1529a035ca
cursosisraelsp <cursos.israelsp@gmail.com>
2026-06-25 16:48:13 +0200

    Funcion suma con valores de entrada

---
+++ Exemplos/Inicio_Modular/Suma_con_valores_de_entrada.cpp
@@ -0,0 +1,24 @@
+// Online C++ Compiler - Build, Compile and Run your C++ programs online in your favorite browser
+
+#include<iostream>
+
+using namespace std;
+
+// DECLARACIÓN DE FUNCIONES
+
+int Suma(int valor1, int valor2);
+
+int main()
+{
+    // Declaración de variables
+    int resultado = 0; // INICIALIZO RESULTADO A 0
+    
+    resultado = Suma(3,14); // UTILIZAMOS UNHA VARIABLE PARA ALAMACENAR O RESULTADO DA SUMA
+    
+    cout << "A Suma é: " << resultado;
+}
+
+int Suma(int valor1, int valor2){
+    
+    return valor1 + valor2;
+}
